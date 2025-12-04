7dfae5136b0796cf42ca8e29e95e777d9652bc8e
AdrianLian <adlia4258@Oslomet.no>
2025-12-04 08:40:45 +0100

    Fiksa homepage og styling2

--- HealthApp/src/shared/NavMenu.tsx
+++ HealthApp/src/shared/NavMenu.tsx
@@ -7,17 +7,19 @@ import AuthSection from '../auth/AuthSection';
 
 const NavMenu: React.FC = () => {
     return (
-        <Navbar expand="lg">
-            <Navbar.Brand href="/">HealthApp</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
-                    <Nav.Link as={Link} to="/bookings">Bookings</Nav.Link>
-                    <Nav.Link as={Link} to="/availableDays">AvailableDays</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            <AuthSection />
+        <Navbar expand="lg" bg="white" className="shadow-sm sticky-top py-3">
+            <Container> 
+                <Navbar.Brand as={Link} to="/" className="fw-bold text-primary">HealthApp</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav>
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/bookings">Bookings</Nav.Link>
+                        <Nav.Link as={Link} to="/availableDays">AvailableDays</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                <AuthSection />
+            </Container> 
         </Navbar>
 
         
